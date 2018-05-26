8b71882dab1960a542abcb300d6a69df5f7b6093
huynhailien299 <1560299@student.hcmus.edu.vn>
2018-05-26 23:58:39 +0700

    abc

--- 1560299_1560400/src/App.js
+++ 1560299_1560400/src/App.js
@@ -1,14 +1,14 @@
-import React from 'react';
+import React from "react";
 import {Route} from "react-router-dom";
 import HomePage from "./components/pages/HomePage";
-import HomePage from "./components/pages/LoginPage";
+import LoginPage from "./components/pages/LoginPage";
 
-const App = () => 
+const App = () => (
 <div>
   <Route path="/" exact component={HomePage}/>
   <Route path="/login" exact component={LoginPage}/>
 
-</div>;
+</div>
 
-
+);
 export default App;
--- 1560299_1560400/src/components/pages/LoginPage.js
+++ 1560299_1560400/src/components/pages/LoginPage.js
@@ -1,10 +1,10 @@
-import React from "react";
+import React from 'react';
 
 
 const LoginPage = () => (
     <div>
         <h1>Login Page</h1>
-        
+       
     </div>
 );
 

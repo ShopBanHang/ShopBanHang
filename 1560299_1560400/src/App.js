1ab34ce4137f782599cdb0f38c3342a9da1c254a
huynhailien299 <1560299@student.hcmus.edu.vn>
2018-06-07 11:03:43 +0700

    Tạo components

--- 1560299_1560400/src/App.js
+++ 1560299_1560400/src/App.js
@@ -1,14 +1,9 @@
-import React from "react";
-import {Route} from "react-router-dom";
-import HomePage from "./components/pages/HomePage";
-import LoginPage from "./components/pages/LoginPage";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
 
-const App = () => (
-<div className="ui container">
-  <Route path="/" exact component={HomePage}/>
-  <Route path="/login" exact component={LoginPage}/>
-
-</div>
-
-);
-export default App;
+it('renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<App />, div);
+  ReactDOM.unmountComponentAtNode(div);
+});
